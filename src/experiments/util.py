fac543055744b2e9e3732787374d9da244c488d1
Benjamin Meyer <meyer@kennwerte.ch>
2021-08-27 16:46:04 +0200

    Add initial experiments

---
+++ src/experiments/util.py
@@ -0,0 +1,22 @@
+import pickle
+from typing import Tuple
+
+import numpy as np
+
+
+def _load(file: str) -> Tuple[np.array, np.array]:
+    with open(file, 'rb') as fo:
+        train_data = pickle.load(fo, encoding='bytes')
+        return train_data['images'], train_data['labels'].flatten()
+
+
+def load_data(data_dir='../../data'):
+    return _load(f'{data_dir}/train.pkl')
+
+
+def load_test(data_dir='../../data'):
+    return _load(f'{data_dir}/test.pkl')
+
+
+def load_data_and_test(data_dir='../../data'):
+    return load_data(data_dir), load_test(data_dir)
