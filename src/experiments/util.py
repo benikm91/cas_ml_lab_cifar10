2795a8702ddd98cb8a7b1540f47b441196ca19c5
Benjamin Meyer <meyer@kennwerte.ch>
2022-12-10 08:33:48 +0100

    Update intro text.

--- src/experiments/util.py
+++ src/experiments/util.py
@@ -7,7 +7,7 @@ import numpy as np
 def _load(file: str) -> Tuple[np.array, np.array]:
     with open(file, 'rb') as fo:
         train_data = pickle.load(fo, encoding='bytes')
-        return train_data['images'], train_data['labels'].flatten()
+        return train_data['images'].astype(np.ubyte), train_data['labels'].flatten()
 
 
 def load_data(data_dir='../../data'):
